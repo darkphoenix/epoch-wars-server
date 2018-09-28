416b60c281d59ae52b93d6ed7748a5588865a936
Daniel Hauck <mail@dhauck.eu>
2018-09-28 23:38:56 +0200

    Build API for map.

--- map.py
+++ map.py
@@ -1,22 +1,43 @@
 from enum import Enum
 
-class Field(Enum):
-    EMPTY = 0
-    HOUSE = 1
-    VILLA = 2
-    TOWER = 3
+class MapError(Exception):
+    pass
+
+class InvalidBuild(MapError):
+    def __init__(self, pos, building):
+        pass
+
+
+class Buliding(Enum):
+    HOUSE = 0
+    VILLA = 1
+    TOWER = 2
 
     def __repr__(self):
-        if self.name == 'EMPTY':
-            return '__'
-        elif self.name == 'HOUSE':
+        if self.building == 'HOUSE':
             return '🏠'
-        elif self.name == 'VILLA':
+        elif self.building == 'VILLA':
             return '🏡'
-        elif self.name == 'TOWER':
+        elif self.building == 'TOWER':
             return '🗼'
         else:
             return '??'
+
+    def __str__(self):
+        return self.__repr__()
+
+class Field():
+    building = None
+    blocked = False
+
+    def __repr__(self):
+        if self.building is None:
+            if self.blocked:
+                return '╳╳'
+            else:
+                return '  '
+        else:
+            return str(self.building)
 
     def __str__(self):
         return self.__repr__()
@@ -29,10 +50,12 @@ class PlayerMap():
         for x in range(size[0]):
             tmp = []
             for y in range(size[1]):
-                tmp.append(Field.EMPTY)
+                tmp.append(Field())
             self.fields.append(tmp)
 
     def display(self):
         for row in self.fields:
             print(*map(str, row), sep='')
 
+    def build(self, pos, building):
+        pass
